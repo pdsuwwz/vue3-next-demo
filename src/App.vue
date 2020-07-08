4509a77baf561efcd5027c06bb5398187f88acf5
pdsuwwz <pdsu.wwz@foxmail.com>
2020-07-09 01:26:55 +0800

    chore: build frame

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Hello {{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      title: 'Vue.js 3.0'
+    }
+  }
+}
+</script>
+<style scoped>
+.home {
+  color: red;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/**
+ * 打包时应注释该 import 语句
+ */
+// import webpack from 'webpack'
+
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const resolve = (dir) => path.join(__dirname, dir)
+
+/**
+ * @type {webpack.Configuration}
+ */
+const config = {
+  mode: process.env.NODE_ENV || 'none',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: resolve('/dist')
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve('template/index.html')
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      clearConsole: false,
+      onErrors (severity, errors) {
+        if (severity !== 'error') return
+        const error = errors[0]
+        notifier.notify({
+          title: 'Webpack error',
+          message: `${severity}: ${error.name}`,
+          subtitle: error.file || '',
+        });
+      },
+    }),
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: 8080,
+    quiet: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.vue'],
+    alias: {
+      '@': resolve('src'),
+      'vue$': 'vue/dist/vue.runtime.esm-bundler.js'
+    }
+  }
+}
+
+module.exports = config
